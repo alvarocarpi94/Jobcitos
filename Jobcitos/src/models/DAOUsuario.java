485b8503702628b9888954cbf90991293dafdcde
Usuario Local <Usuario Local>
2018-05-14 14:19:13 +0200

    clases dao y transfer de usuario

---
+++ Jobcitos/src/models/DAOUsuario.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.ArrayList;
+
+import controllers.TransferUsuario;
+
+public class DAOUsuario {
+	//
+	ArrayList<TransferUsuario> listaUsuario;
+	
+	public DAOUsuario(){
+		listaUsuario = new ArrayList<TransferUsuario>();
+		TransferUsuario usu1 = new TransferUsuario();
+		TransferUsuario usu2 = new TransferUsuario();
+		listaUsuario.add(usu1);
+		listaUsuario.add(usu2);
+	}
+	
+	public ArrayList<TransferUsuario> getListaUsuario(){
+		return listaUsuario;
+	}
+	
+	public TransferUsuario getUsuario(int i){
+		return listaUsuario.get(i);
+	}
+	
+	public void updateUsuario(TransferUsuario usu){
+		/*listaUsuario.get(usu.getid()).setNombreUsuario();
+		listaUsuario.get(usu.getid()).setContraseña();*/
+		
+	}
+	
+
+
+}
