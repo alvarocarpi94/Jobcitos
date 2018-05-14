40393ded652f8ae8289bb4950f83e539c8bbd6cf
Usuario Local <Usuario Local>
2018-05-14 16:55:37 +0200

    cambio de nombre de carpetas

---
+++ Jobcitos/src/negocio/Usuario.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import java.util.ArrayList;
+
+public class Usuario {
+	
+//Atributos
+public String oferta;	
+
+//metodos
+public Usuario(){}
+
+void buscarOferta(){}
+void darseBaja(){}
+void crearCuenta(){}
+void EnviarMensaje(){}
+
+}
