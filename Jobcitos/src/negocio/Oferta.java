52c627dd7f9b719ef78e954cb8e6c31cc1b1cd5d
usuario_local <usuario_local@PTO0220>
2018-05-17 18:13:51 +0200

    recolocacion de clases y se añade la clase utils y exccepciones

--- Jobcitos/src/negocio/Oferta.java
+++ Jobcitos/src/negocio/Oferta.java
@@ -1,41 +1,15 @@
-package negocio;
+package oferta;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Oferta {
-    private ArrayList<String> listaCandidatos;
-    private String Ofertante;
-    private String Trabajador;
-    private String Lugar;
-    private boolean pagado;
-
-    public Oferta(String Ofertante,String lugar){
-        this.listaCandidatos = new ArrayList<String>();
-        this.Ofertante = Ofertante;
-        this.Trabajador = null;
-        this.Lugar = lugar;
-        this.pagado = false;
-    }
-
-    public boolean eliminarTrabajador(){
-        if(this.Trabajador != null) {
-            this.Trabajador = null;
-            return true;
-        }else return false;
-    }
-
-    public boolean aniadirTrabajador(String idTrabajador){
-        if(this.Trabajador != null) return false;
-        else{
-        	this.Trabajador = idTrabajador;
-        	return true;
-        }
-    }
-
-    public void ofertaPagada(){
-        this.pagado = true;
-    }
-
-    public void aniadirCandidato(String trabajador){
-        this.listaCandidatos.add(trabajador);
-    }
+	
+	private TransferOferta to;
+	
+	
+	/*public Oferta(TransferOferta to) {
+		super(to.g);
+	}*/
+	
 }
