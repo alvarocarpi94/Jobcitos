b05aefa5afc83d67e43cb7d139f3ce0a2fa7063f
Yiiiteng <35228825+Yiiiteng@users.noreply.github.com>
2018-05-20 16:57:54 +0200

    Add files via upload

--- Jobcitos/src/negocio/Oferta.java
+++ Jobcitos/src/negocio/Oferta.java
@@ -1,15 +1,47 @@
 package negocio;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Oferta {
-	
-	private TransferOferta to;
-	
-	
-	/*public Oferta(TransferOferta to) {
-		super(to.g);
-	}*/
-	
+    private ArrayList<String> listaCandidatos;
+    private String Ofertante;
+    private String Trabajador;
+    private String Lugar;
+    private boolean pagado;
+
+    public Oferta(String Ofertante,String lugar){
+        this.listaCandidatos = new ArrayList<String>();
+        this.Ofertante = Ofertante;
+        this.Trabajador = null;
+        this.Lugar = lugar;
+        this.pagado = false;
+    }
+
+    public boolean eliminarTrabajador(){
+        if(this.Trabajador != null) {
+            this.Trabajador = null;
+            return true;
+        }else return false;
+    }
+
+    public boolean añadirTrabajador(String idTrabajador){
+        if(this.Trabajador != null) return false;
+        else{
+        	//TransferOferta transferO = new TransferOferta(); 
+        	this.Trabajador = idTrabajador;
+        	//transferO.setTrabajador(this.Trabajador);
+        	return true;
+        }
+    }
+
+    public void ofertaPagada(){
+    	//TransferOferta transferO = new TransferOferta(); 
+        this.pagado = true;
+      //transferO.setPagada(this.pagada);
+    }
+
+    public void añadirCandidato(String trabajador){
+    	//TransferOferta transferO = new TransferOferta(); 
+        this.listaCandidatos.add(trabajador);
+      //transferO.setCanditato(this.listaCandidatos);
+    }
 }
