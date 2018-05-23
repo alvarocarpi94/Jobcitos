18d08011fa93dd37ba9b0990215de6aed8d209fb
Yiiiteng <35228825+Yiiiteng@users.noreply.github.com>
2018-05-23 18:42:10 +0200

    Add files via upload

--- Jobcitos/src/negocio/SA.java
+++ Jobcitos/src/negocio/SA.java
@@ -6,64 +6,29 @@ import integracion.DatosOferta;
 import integracion.DatosUsuario;
 
 public class SA {
-	DAOOferta daoO;
-	public SA(){
-		//this.daoO = new DAOOferta();
-	}
 	
-	public void crearOferta(String nom, String desc, String loc, String of, String trab, String cand, double prAc, boolean p) throws InconsistenciaDeDatos{
+	public void crearOferta(String nom, String desc, String loc, String of, double prAc) throws InconsistenciaDeDatos {
+		Oferta oferta = new Oferta(nom, desc, loc, of, prAc);
+		oferta.crearOferta();
 		
-		TransferOferta transO = new TransferOferta(nom,desc,loc,of,null,null,prAc,false);
-		try {
-			this.daoO.insertarOferta(transO);
-		}catch (InconsistenciaDeDatos error){
-			//que tipo de error es ??
-		}
 	}
 	
-	public void borrarOferta(Oferta oferta) throws InconsistenciaDeDatos{
-		String nom = oferta.getNombre();
-		String of = oferta.getOfertante();
-		TransferOferta t = new TransferOferta();
-		t.setNombre(nom);
-		t.setOfertante(of);
-		try {
-			this.daoO.eliminarOferta(t);
-		} catch (InconsistenciaDeDatos e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void borrarOferta(Oferta oferta) {
+		oferta.borrarOferta();
 	}
 	
 	public void valorarTrabajador(int nota){
 		
 	}
 	public void despedirTrabajador(Oferta oferta){
-		String nom = oferta.getNombre();
-		String of = oferta.getOfertante();
-		String trabajador = oferta.getTrabajador();
-		TransferOferta t = new TransferOferta();
-		t.setNombre(nom);
-		t.setOfertante(of);
-		t.setTrabajadorContratado(trabajador);
 		oferta.eliminarTrabajador();
-		try {
-			this.daoO.eliminarTrabajador(t);
-		} catch (InconsistenciaDeDatos e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 	}
 	public void contratarTrabajador(Oferta oferta,String idTrabajador){
 		oferta.aniadirTrabajador(idTrabajador);
 	}
-	public void editarOferta(TransferOferta tOferta) throws InconsistenciaDeDatos, IOException{
-		DatosOferta daoOfer = new DatosOferta(fileOfertas);
-		daoOfer.cargarOfertas();
-		if(daoOfer.obtenerOferta(tOferta.getId()).equals(tOferta.getId())) {
-			daoOfer.reemplazarOferta(tOferta.getId(), tOferta);
-		}
+	public void editarOferta(TransferOferta tOferta,Oferta oferta) throws InconsistenciaDeDatos{
+		oferta.modificar(tOferta);
 	}
 	
 	public void editarPerfil(TransferUsuario tUser){
@@ -77,7 +42,6 @@ public class SA {
 	}
 	
 	public void aceptarOferta(String trab,Oferta oferta){
-		
 		oferta.aniadirCandidato(trab);
 	}
 	
