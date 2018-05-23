9161043de6efa39c07548d883dc97b0ade2cbb82
aliaspseudonimo <39057532+aliaspseudonimo@users.noreply.github.com>
2018-05-23 18:56:20 +0200

    Add files via upload

--- Jobcitos/src/utils/MyStringUtils.java
+++ Jobcitos/src/utils/MyStringUtils.java
@@ -1,16 +1,16 @@
-package utils;
+package util;
 
 public class MyStringUtils {
 	
-	private static final String cadenaComposicion = "-";
 	private static final String cadenaSeparadora = "/";
+	private static final String cadenaJuntarElementos = ";";
 	
-	public static String componerCadena(String cad1, String cad2){
-		return cad1 + MyStringUtils.cadenaComposicion + cad2;
+	public static String ponerSeparadorEntreCadenas(String cad1, String cad2){
+		return cad1 + MyStringUtils.cadenaSeparadora + cad2;
 	}
 	
-	public static String juntarCadenasConSeparador(String cad1, String cad2){
-		return cad1 + MyStringUtils.cadenaSeparadora + cad2;
+	public static String juntarElementos(String elem1, String elem2){
+		return elem1 + MyStringUtils.cadenaJuntarElementos + elem2;
 	}
 
 }
