6c21912b59c2575eec60c81459309b0df07b132d
aliaspseudonimo <39057532+aliaspseudonimo@users.noreply.github.com>
2018-05-23 18:52:20 +0200

    Add files via upload

--- Jobcitos/src/integracion/DAOOferta.java
+++ Jobcitos/src/integracion/DAOOferta.java
@@ -1,17 +1,12 @@
-package integracion;
+package basededatos;
 
-import java.io.IOException;
-
-import excepciones.InconsistenciaDeDatos;
-import negocio.TransferOferta;
+import oferta.TransferOferta;
 
 public interface DAOOferta {
 	
-	public void cargarOfertas() throws IOException;
-	public void guardarOfertas() throws IOException;
-	public void insertarOferta(TransferOferta to) throws InconsistenciaDeDatos;
-	public void eliminarOferta(TransferOferta to) throws InconsistenciaDeDatos;
-	public void reemplazarOferta(String id, TransferOferta to) throws InconsistenciaDeDatos;
-	public TransferOferta obtenerOferta(String id) throws InconsistenciaDeDatos;
+	public void insertarOferta(TransferOferta to);
+	public void eliminarOferta(String idTransferOferta);
+	public void reemplazarOferta(String idTransOf, TransferOferta reemplazo);
+	public TransferOferta obtenerOferta(String idTransferOferta);
 
 }
---
+++ Jobcitos/src/integracion/ImplDAOOferta.java
@@ -0,0 +1,73 @@
+package basededatos;
+
+// import java.io.BufferedReader;
+// import java.io.File;
+// import java.io.FileInputStream;
+// import java.io.FileOutputStream;
+import java.io.IOException;
+//import java.io.InputStreamReader;
+// import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import oferta.TransferOferta;
+
+public class ImplDAOOferta implements DAOOferta{
+	
+	private static ImplDAOOferta instancia=null;
+	// private String nombreFichero = "Ofertas.txt";
+	// private File ficheroOfertas;
+	private Map<String, String> mapaOfertas;
+	
+	private ImplDAOOferta() {
+		// this.ficheroOfertas = new File(this.nombreFichero);
+		this.mapaOfertas = new HashMap<String, String>();
+	}
+	
+	public static ImplDAOOferta getInstanceOfImplDAOOferta() {
+		if(ImplDAOOferta.instancia == null)
+			return new ImplDAOOferta();
+		
+		return ImplDAOOferta.instancia;
+	}
+	
+	public void cargarOfertasEnMapa() throws IOException{
+		// BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(this.ficheroOfertas)));
+	}
+
+	public void guardarOfertasEnFichero() throws IOException {
+		// PrintWriter pw = new PrintWriter(new FileOutputStream(this.ficheroOfertas));
+		
+	}
+	
+	public void insertarOferta(TransferOferta to){
+		if(this.mapaOfertas.get(to.getId()) == null)
+			this.guardarOferta(to);
+	}
+	
+	public void eliminarOferta(String idTransferOferta){
+		this.mapaOfertas.remove(idTransferOferta);
+	}
+	
+	public void reemplazarOferta(String idTransOf, TransferOferta reemplazo){
+		this.mapaOfertas.remove(idTransOf);
+		this.guardarOferta(reemplazo);
+	}
+	
+	public TransferOferta obtenerOferta(String idTransferOferta){
+		if(this.mapaOfertas.containsKey(idTransferOferta)){
+			return this.cargarOferta(this.mapaOfertas.get(idTransferOferta));
+		}
+		
+		return null;
+	}
+	
+	private TransferOferta cargarOferta(String textoTransferOferta){
+		return null;
+	}
+	
+	private void guardarOferta(TransferOferta to){
+		
+	}
+	
+}
