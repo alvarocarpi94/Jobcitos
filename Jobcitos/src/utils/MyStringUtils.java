b4444f3a9b8a8f8e9f10d35ebb8977c67441c6bf
aliaspseudonimo <39057532+aliaspseudonimo@users.noreply.github.com>
2018-05-24 18:57:44 +0200

    Add files via upload
    Pequeñas modificaciones en MyStringUtils que ayudan en el manejo de datos y ficheros

--- Jobcitos/src/utils/MyStringUtils.java
+++ Jobcitos/src/utils/MyStringUtils.java
@@ -1,9 +1,18 @@
-package utils;
+package util;
 
 public class MyStringUtils {
 	
 	private static final String cadenaSeparadora = "/";
 	private static final String cadenaJuntarElementos = ";";
+	private static final String cadenaIdCompuesto = "-";
+	
+	public static String getCadenaSeparadora(){
+		return MyStringUtils.cadenaSeparadora;
+	}
+	
+	public static String getCadenaJuntarElementos(){
+		return MyStringUtils.cadenaJuntarElementos;
+	}
 	
 	public static String ponerSeparadorEntreCadenas(String cad1, String cad2){
 		return cad1 + MyStringUtils.cadenaSeparadora + cad2;
@@ -11,6 +20,10 @@ public class MyStringUtils {
 	
 	public static String juntarElementos(String elem1, String elem2){
 		return elem1 + MyStringUtils.cadenaJuntarElementos + elem2;
+	}
+	
+	public static String crearIdCompuesto(String elem1, String elem2){
+		return elem1.replaceAll("\\s+", MyStringUtils.cadenaJuntarElementos) + MyStringUtils.cadenaIdCompuesto + elem2.replaceAll("\\s+", MyStringUtils.cadenaJuntarElementos);
 	}
 
 }
