c39ad18a64ed8a930eceda5b4c042a97c0f77704
aliaspseudonimo <39057532+aliaspseudonimo@users.noreply.github.com>
2018-05-25 19:54:24 +0200

    Add files via upload

--- Jobcitos/src/presentacion/Controlador.java
+++ Jobcitos/src/presentacion/Controlador.java
@@ -1,56 +1,79 @@
 package presentacion;
 
+import integracion.DAOOferta;
+import integracion.DAOUsuario;
+import integracion.ImpDAOOferta;
+
+import java.util.List;
+
+import util.MyStringUtils;
+import negocio.SA;
+import negocio.TransferOferta;
+import negocio.TransferUsuario;
+import negocio.Usuario;
+
 public class Controlador {
 	
-	public void userButtonCrearOferta() {
-		
+	private SA servicioAplicacion;
+	
+	public Controlador(){
+		this.servicioAplicacion = new SA();
 	}
 	
-	public void userButtonBorrarOferta() {
-		
+	public void userButtonCrearOferta(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p) {
+		this.servicioAplicacion.crearOferta(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p));
 	}
 	
-	public void userActionValorarTrabajador() {
-		
+	public void userButtonBorrarOferta(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p) {
+		this.servicioAplicacion.borrarOferta(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p));
 	}
 	
-	public void userButtonDespedirTrabajador() {
-		
+	public void userActionValorarTrabajador(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p, int val) {
+		this.servicioAplicacion.valorarTrabajador(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p), val);
 	}
 	
-	public void userButtonContratarTrabajador() {
-		
+	public void userButtonDespedirTrabajador(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p) {
+		this.servicioAplicacion.despedirTrabajador(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p));
 	}
 	
-	public void userActionEditarOferta(){
-		
+	public void userButtonContratarTrabajador(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p, String idTrabajador) {
+		this.servicioAplicacion.contratarTrabajador(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p), idTrabajador);
 	}
 	
-	public void userActionEditarPerfil(){
-		
+	public void userActionEditarOferta(String idAntiguo, String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p){
+		this.servicioAplicacion.editarOferta(idAntiguo, new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p));
 	}
 	
-	public void userButtonPagarOferta(){
-		
+	/*	public void userButtonBuscarOferta(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p){
+		this.servicioAplicacion.buscarOferta(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p));
+	}*
+	
+	public void userButtonPagarOferta(String tit, String desc, String loc, String of, String trab, List<String> cand, double prAc, boolean p){
+		this.servicioAplicacion.pagarOferta(new TransferOferta(MyStringUtils.crearIdCompuesto(of, tit), tit, desc, loc, of, trab, cand, prAc, p));
 	}
 	
-	public void userButtonretirarse(){
-		
+	public void userActionEditarPerfil(String nombre, String apellidos, String correo, String contrasena, Integer[] mediaO, Integer[] mediaT, String listaOfertas){
+		this.servicioAplicacion.editarPerfil(new TransferUsuario(correo, contrasena, nombre, apellidos, mediaO, mediaT, listaOfertas));
+	}
+	
+	public void userButtonRetirarseOferta(){
+		this.servicioAplicacion.retirarse(tOferta, tUser);
 	}
 	
 	public void userActionValorarOfertante() {
 		
 	}
+
+	public void userButtonDarseBaja(String idTransferUsuario){
+
+		this.servicioAplicacion.buscarOferta(tOferta, tUser);
+	} */
 	
-	public void userButtonBuscarOferta(){
+	public void userButtonCrearCuenta(String nombre, String apellidos, String correo, String contrasena){
+		TransferUsuario tu;
+		Integer[] valoracionesIniciales = {0, 0};
 		
-	}
-	
-	public void userButtonDarseBaja(){
-		
-	}
-	
-	public void userButtonCrearCuenta(){
-		
+		tu = new TransferUsuario(correo, contrasena, nombre, apellidos, valoracionesIniciales, valoracionesIniciales, "");
+		this.servicioAplicacion.crearCuenta(tu);
 	}
 }
