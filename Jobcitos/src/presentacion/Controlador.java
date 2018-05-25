1eeadfc5a7bb8744f17ffb4533edd6d382151533
-V <-V@DESKTOP-34341SG>
2018-05-25 17:21:03 +0200

    Clase controlador

---
+++ Jobcitos/src/presentacion/Controlador.java
@@ -0,0 +1,56 @@
+package presentacion;
+
+public class Controlador {
+	
+	public void userButtonCrearOferta() {
+		
+	}
+	
+	public void userButtonBorrarOferta() {
+		
+	}
+	
+	public void userActionValorarTrabajador() {
+		
+	}
+	
+	public void userButtonDespedirTrabajador() {
+		
+	}
+	
+	public void userButtonContratarTrabajador() {
+		
+	}
+	
+	public void userActionEditarOferta(){
+		
+	}
+	
+	public void userActionEditarPerfil(){
+		
+	}
+	
+	public void userButtonPagarOferta(){
+		
+	}
+	
+	public void userButtonretirarse(){
+		
+	}
+	
+	public void userActionValorarOfertante() {
+		
+	}
+	
+	public void userButtonBuscarOferta(){
+		
+	}
+	
+	public void userButtonDarseBaja(){
+		
+	}
+	
+	public void userButtonCrearCuenta(){
+		
+	}
+}
